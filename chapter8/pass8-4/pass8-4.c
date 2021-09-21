a2c00eff0f238d2fed01072169f7777e6e072b59
testarossa0214 <soundlight@live.jp>
2021-09-21 15:50:11 +0900

    chapter9

--- chapter8/pass8-4/pass8-4.c
+++ chapter8/pass8-4/pass8-4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void) {
 	// ここにコードを挿入
-	int times[]={1, 2, 3, 4, 5};
+//	int times[]={1, 2, 3, 4, 5};
 	int oubo[]={150, 220, 185, 210, 190};
 	int saiyou[]={25, 30, 24, 36, 33};
 	for (int i=0; i<5; i++){
